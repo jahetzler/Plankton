173819a3602e91d0d641e2f2da6b53bd4b98741f
jahetzler <43454480+jahetzler@users.noreply.github.com>
2018-09-20 22:57:05 +0200

    Create data_analysis.r

---
+++ data_analysis.r
@@ -0,0 +1,8 @@
+# x = species counted, function for copipoda pr m^3 
+sample_density = function(x) {
+  x <- (x * 200)/18.75# 200ml sample from half of the sample (2x), devided by 
+  
+    return(x)
+  }
+
+sample_density(6)
