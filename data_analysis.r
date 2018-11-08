752eb378b0b3cdcdb3f832719b064beca860cc90
jahetzler <43454480+jahetzler@users.noreply.github.com>
2018-11-08 15:16:18 +0100

    Update data_analysis.r

--- data_analysis.r
+++ data_analysis.r
@@ -20,14 +20,14 @@ library(effects)
 DATA0 <- read.csv("Zooplankton_data.csv", head = TRUE)
 View(DATA0)
 
-
+#Run this code for scaling of data
 MEAN <-function(y){do.call(rbind,as.list(by(y,DATA0$Site,mean)))} #new dataframe based on the mean of the sub samples
 DATA_mean <- apply(DATA0[,4:17],2,MEAN)
 DATA_temp <- (DATA0[c(1,4,7,10,13,15),c(1,2,3,18,19,20)])
 DATA <- cbind(DATA_temp, DATA_mean)
 rm(DATA_temp, DATA_mean, MEAN) #Clean up environment
 View(DATA)
-a <- DATA
+
 
 #watercolumn sample function x/m^3
 sample_density_cop = function(x) {
@@ -50,125 +50,37 @@ View(CTD)
 
 CTD_mean <- aggregate(. ~Ser, data=CTD, mean, na.rm=TRUE)
 View(CTD_mean)
-CTD_mean <- aggregate(. ~Ser, data=CTD, mean, na.rm=TRUE)
-View(CTD_mean)
-
-#FIND CODE FOR DELETING DEPTH AFTER BOTTOM IS REACHED, TEMP SOLUTION: CHANGE DEPTH MANUALY IN TXT FILE
-
 
 #Taxanomic plot ####
 library(reshape)
-b <- melt(DATA[,-c(3:6,1,8,20)])
+b <- melt(DATA[,-c(3,5:7,1,20)])
 
 ggplot(data = b, aes(x=Site, y= value/100 , fill=variable)) + 
   geom_bar(stat="identity",position="fill", colour="black") +
   labs(y = "Relative abundance")
 
+ggplot(data = b, aes(x="", fill=variable)) + 
+  geom_bar(position="fill", colour="black") +
+  labs(y = "Relative abundance")+ 
+  facet_grid(. ~ Fjord)+
+  coord_polar(theta = "y")
 
 #CTD plots ####
 
 
-ggplot(data = CTD, aes(x = Seq, y = Temp, by = Ser, color = Ser)) + 
-  #geom_point() + 
-  #geom_line() + 
-  geom_smooth() + 
-  labs(x = "Time in sec")
-
+#
 ggplot(data = CTD, aes(x = Depth, y = Temp, by = Ser, color = Ser)) + 
-  #geom_point() + 
   geom_line() +
-  #geom_smooth() + 
   labs(x = "Depth", color = "Sites") + 
   facet_grid(. ~ Fjord)
 
 
-#DATA ANALYSIS ####
-plot(DATA0[,-c(1,5:16,19:20)])#general plot with copepods_all and zooplankton_all and without coordinates, ID
-
-
-#simple linear regression ####
-fit = lm(data=DATA0, Biomass ~ Copepods)
-plot(DATA0$Biomass ~ DATA0$Copepods, xlim = c(0,450), ylim=c(1500,2400))
-abline(fit, col="green")
-
-
-cor.test(DATA0$Copepods, DATA0$Biomass)#seems to be correlation between biomass and copepods counted
-
-
-plot(DATA0$Copepods ~ DATA0$Site)#sample summary
-
-fit = lm(data = DATA0, Zooplankton_all ~ Copepods)
-plot(data = DATA0, Zooplankton_all ~Copepods, xlim = c(0,450), ylim=c(0,800))
-abline(fit, col="gray")
-#COMPARE ZOOPLANKTON_ALL TO BIOMASS WITH THE SAME TEST AS ABOVE
-
+ggplot(data = CTD, aes(x = Temp, y = Fluoride, by = Ser, color = Ser))+
+  geom_point()+
+  facet_grid(. ~ Ser) +
+  ylim(0,3)
 
 #MAP ####
 pointLabels <- annotate("text",x=DATA0$Lon,y=c(DATA0$Lat),size=3,label=as.vector(DATA0$Site))
 map <- qmplot(Lon, Lat, data = DATA0, zoom = 10, maptype = "toner-lite", color = I("red")) + pointLabels
 map
-
-#Difference in Biomass between fjord ####
-plot(DATA$Biomass ~ DATA$Fjord)
-plot(DATA$Copepods ~DATA$Fjord)
-
-wilcox.test(DATA$Biomass~DATA$Fjord, paired=F, exact=T)
-
-mean_data <- aggregate(cbind(Biomass, Copepods) ~ Fjord, data = DATA, sum)
-t.data.frame(mean_data)
-
-sample_density_bio(mean_data$Biomass) #total Biomass mg/m^3 in the fjords
-
-
-#ANOVA Test Biomass between fjords ####
-#W shapiro test
-fit <- aov(Biomass ~ Fjord, data = DATA)
-summary.lm(fit)
-summary(fit)
-
-boxplot(Biomass~Fjord, data=DATA,ylab="Biomass",col="lightblue")
-
-leveneTest(Biomass~Fjord, data=DATA) # Levene's test, F=6.3527, p=0.002259, i.e. variances significantly non-homogenous
-max(by(DATA$Biomass,DATA$Fjord,sd))^2/min(by(DATA$Biomass,DATA$Fjord,sd))^2 # 3.25, a rule of thumb is that if the ratio of the highest to the lowest variance is < 5, variances are sufficiently close to equality
-# since Levene's test indicates a significant deviation from homogeneity, we can use
-car::Anova(fit,type="III",white.adjust=TRUE) 
-
-#ANOVA test difference for sampling sites ####
-fit <- aov(Zooplankton_all ~ Site, data = DATA0)
-summary.lm(fit)
-summary(fit)
-
-plot(allEffects(fit,confidence.level = 0.95))
-boxplot(Zooplankton_all~Site, data=DATA0,ylab="Biomass",col="lightblue")
-
-#errr, 2-way anova zooplankton_all by fjord and biomass
-fit <- aov(Zooplankton_all ~ Fjord + Biomass, data = DATA)
-summary(fit)
-
-plot(fit)
-plot(allEffects(fit,confidence.level = 0.95))
-
-
-#WRONG TEST - test for difference in two different sites, use students t-test ####
-hist(DATA$Biomass[DATA$Fjord == "Mistfjord"], freq = FALSE, xlab = "Biomass", main = "Distribution of Biomass", col = "lightgray", xlim = c(-50,850), ylim = c(0,0.008),)
-hist(DATA$Biomass[DATA$Fjord == "Nordfjord"], add = T, freq = FALSE, col = "darkgray", xlim = c(1600,2300), ylim = c(0,0.008))
-curve(dnorm(x, mean=mean(DATA[1:3,3]), sd = sd(DATA[1:3,3])), add = TRUE, col="Orange", lwd = 2)
-curve(dnorm(x, mean=mean(DATA[4:6,3]), sd = sd(DATA[4:6,3])), add = TRUE, col="black", lwd = 2)
-
-#t-test mess 
-mean(DATA[1:3,3])#Mistfjord
-mean(DATA$Biomass[DATA$Fjord == "Mistfjord"])
-sd(DATA[1:3,3])#Mistfjord
-var(DATA[1:3,3])#Mistfjord
-
-mean(DATA[4:6,3])#Nordfjord
-sd(DATA0[4:6,3])#Nordfjord
-var(DATA0[4:6,3])#Nordfjord
-
-plot(DATA$Biomass ~ DATA$Fjord)
-
-a <- t.test(Biomass ~ Fjord, data = DATA)#more noise than signal t < 1
-str(a)
-
-t.test(Biomass ~ Sample == c("M1", "M2", "M4"), data = DATA)#t-test for inside the fjords
-
